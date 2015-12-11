cf7054d4f595f1aa414d4d5db1a1aa7279841e0f
Mark Birbeck <mark.birbeck@gmail.com>
2015-12-11 16:35:16 +0000

    add tests

---
+++ test/blog.js
@@ -0,0 +1,71 @@
+'use strict';
+
+let chai = require('chai');
+chai.use(require('chai-as-promised'));
+chai.should();
+
+require('..' /*webdriver-mocha*/);
+
+describe('check my blog', function() {
+  this.timeout(10000);
+
+  describe('each test has its own promise', () => {
+    it('should have the correct title', done =>
+      driver.get('http://markbirbeck.com/')
+      .then(
+        driver.getTitle()
+        .should.eventually.equal('Mark Birbeck\'s Blog')
+      )
+      .then(() => done())
+    );
+
+    it('should click link to open a specific blog post and then have the correct title', done =>
+      driver.get('http://markbirbeck.com/')
+      .then(
+        driver.getTitle()
+        .should.eventually.equal('Mark Birbeck\'s Blog')
+      )
+      .then(
+        driver.findElement(
+          By.linkText('A Mixin Approach to Material Design Lite Using Sass')
+        ).click()
+      )
+      .then(
+        driver.getTitle()
+        .should.eventually.equal('A Mixin Approach to Material Design Lite Using Sass')
+      )
+      .then(() => done())
+    );
+  });
+
+  describe('each test shares a promise', () => {
+    let blog = driver.get('http://markbirbeck.com/');
+
+    it('should have the correct title', done =>
+      blog
+      .then(
+        driver.getTitle()
+        .should.eventually.equal('Mark Birbeck\'s Blog')
+      )
+      .then(() => done())
+    );
+
+    it('should click link to open a specific blog post and then have the correct title', done =>
+      blog
+      .then(
+        driver.getTitle()
+        .should.eventually.equal('Mark Birbeck\'s Blog')
+      )
+      .then(
+        driver.findElement(
+          By.linkText('A Mixin Approach to Material Design Lite Using Sass')
+        ).click()
+      )
+      .then(
+        driver.getTitle()
+        .should.eventually.equal('A Mixin Approach to Material Design Lite Using Sass')
+      )
+      .then(() => done())
+    );
+  });
+});
